2cbc3395a18e6fdfc627231648d57fb8a39e3aa6
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-10-28 09:49:07 +0100

    added latitude longitude fields

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -69,6 +69,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:text)
+      params.require(:link).permit(:text, :latitude, :longitude)
     end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,8 @@ class Link
   include Mongoid::Document
 
   field :text, type: String
+  field :latitude, type: Float
+  field :longitude, type: Float
 
   def relevance
     self.created_at
