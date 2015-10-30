17d8b0d819325229bf7466a069e5ef7bc1d244d7
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-10-30 22:18:44 +0100

    location request

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -74,6 +74,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:text, :latitude, :longitude)
+      params.require(:link).permit(:text).merge(@context)
     end
 end
