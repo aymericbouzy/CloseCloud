3d560975b07d0840135a387a1a0386b0d4d02ba1
Aymeric Bouzy <aymeric.bouzy@gmail.com>
2015-11-05 00:19:00 +0100

    bug corrections

--- app/models/link.rb
+++ app/models/link.rb
@@ -17,7 +17,22 @@ class Link
 
   def self.nearby(context)
     links = Link.all.select { |l| l.geocoded? && l.distance_from([context[:latitude], context[:longitude]]) < Link.max_radius }
-    google_places_result = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCSdMKcdPKJbzYunFO7-15dCZdEwkYPDVM&location=#{context[:latitude]},#{context[:longitude]}&radius=#{Link.max_radius}")
+    types = ["accounting", "airport", "amusement_park", "aquarium", "art_gallery", "atm", "bakery",
+      "bank", "bar", "beauty_salon", "bicycle_store", "book_store", "bowling_alley", "bus_station",
+      "cafe", "campground", "car_dealer", "car_rental", "car_repair", "car_wash", "casino", "cemetery",
+      "church", "city_hall", "clothing_store", "convenience_store", "courthouse", "dentist",
+      "department_store", "doctor", "electrician", "electronics_store", "embassy", "establishment",
+      "finance", "fire_station", "florist", "food", "funeral_home", "furniture_store", "gas_station",
+      "general_contractor", "grocery_or_supermarket", "gym", "hair_care", "hardware_store", "health",
+      "hindu_temple", "home_goods_store", "hospital", "insurance_agency", "jewelry_store", "laundry",
+      "lawyer", "library", "liquor_store", "local_government_office", "locksmith", "lodging",
+      "meal_delivery", "meal_takeaway", "mosque", "movie_rental", "movie_theater", "moving_company",
+      "museum", "night_club", "painter", "park", "parking", "pet_store", "pharmacy", "physiotherapist",
+      "place_of_worship", "plumber", "police", "post_office", "real_estate_agency", "restaurant",
+      "roofing_contractor", "rv_park", "school", "shoe_store", "shopping_mall", "spa", "stadium",
+      "storage", "store", "subway_station", "synagogue", "taxi_stand", "train_station", "travel_agency",
+      "university", "veterinary_care", "zoo"]
+    google_places_result = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyCSdMKcdPKJbzYunFO7-15dCZdEwkYPDVM&location=#{context[:latitude]},#{context[:longitude]}&radius=#{Link.max_radius * 1000}&types=#{types.join('|')}")
     google_places = JSON.parse(google_places_result.body)["results"].collect { |r|
       Link.new({ text: r["name"], coordinates: [r["geometry"]["location"]["lng"], r["geometry"]["location"]["lat"]], place_id: r["place_id"], origin: "google_places" })
     }
